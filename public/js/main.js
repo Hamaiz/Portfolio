eed6c8ede45bafb2dcfa9b7f673d0c0396a51a3f
Hamaiz <hamaiz5502@gamil.com>
2020-03-15 01:55:31 +0500

    Mobile Home

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,59 @@
+//Navbar
+let body = document.querySelector(".page")
+const menuToggle = document.querySelector(".menu_toggle")
+const content = document.querySelector(".main")
+
+menuToggle.addEventListener("click", () => {
+    body.classList.toggle("shazam")
+})
+content.addEventListener("click", () => {
+    body.classList.remove('shazam')
+})
+
+
 //Mouse Cursor
 let mouseCursor = document.querySelector(".cursor")
 let navList = document.querySelectorAll(".nav_list li a")
 let buttons = document.querySelectorAll(".buttons a")
 let rectangle = document.querySelector(".rectangle")
+let contactInfo = document.querySelector(".contact-info_wrap")
 let socialIcons = document.querySelectorAll(".footer_social-icons a i")
 let jsHover = document.querySelectorAll(".js-hover a")
+let inputBox = document.querySelector("#name")
+let inputBox2 = document.querySelector("#email")
+let textBox = document.querySelector("textarea")
+let buttonContact = document.querySelector("button")
+// let isMobile = window.matchMedia("only screen and (max-width: 991px)")
 
-
+// if (isMobile.matches === false) {
 window.addEventListener("mousemove", cursor)
 function cursor(e) {
     mouseCursor.style.top = e.pageY + 'px'
     mouseCursor.style.left = e.pageX + 'px'
 }
+// }
+if (rectangle) {
+    window.addEventListener("mousemove", ({ x, y }) => {
+        const widthPage = window.innerWidth
+        const heightPage = window.innerHeight
 
-window.addEventListener("mousemove", ({ x, y }) => {
-    const widthPage = window.innerWidth
-    const heightPage = window.innerHeight
+        const traX = (-(widthPage / 2) + x) / 30
+        const traY = ((heightPage / 2) - y) / 30
 
-    const traX = (-(widthPage / 2) + x) / 30
-    const traY = ((heightPage / 2) - y) / 30
+        rectangle.style.transform = `rotateY(${traX}deg) rotateX(${traY}deg) translateZ(-150px)`
+    })
+}
+if (contactInfo) {
+    window.addEventListener("mousemove", ({ x, y }) => {
+        const widthPage = window.innerWidth
+        const heightPage = window.innerHeight
 
-    rectangle.style.transform = `rotateY(${traX}deg) rotateX(${traY}deg) translateZ(-150px)`
-})
+        const traX = (-(widthPage / 2) + x) / 120
+        const traY = ((heightPage / 2) - y) / 120
 
+        contactInfo.style.transform = `rotateY(${traX}deg) rotateX(${traY}deg) translateZ(-150px)`
+    })
+}
 navList.forEach(link => {
     link.addEventListener("mouseleave", _ => {
         mouseCursor.classList.remove("link-grow")
@@ -75,3 +106,52 @@ const swiper = new Swiper(".about-slider", {
         clickable: true
     }
 })
+
+
+//Mouse Hover
+if (inputBox) {
+    inputBox.addEventListener("mouseleave", () => {
+        mouseCursor.classList.remove("link-grow")
+    })
+    inputBox.addEventListener("mouseover", () => {
+        mouseCursor.classList.add("link-grow")
+    })
+    inputBox2.addEventListener("mouseleave", () => {
+        mouseCursor.classList.remove("link-grow")
+    })
+    inputBox2.addEventListener("mouseover", () => {
+        mouseCursor.classList.add("link-grow")
+    })
+    textBox.addEventListener("mouseleave", () => {
+        mouseCursor.classList.remove("link-grow")
+    })
+    textBox.addEventListener("mouseover", () => {
+        mouseCursor.classList.add("link-grow")
+    })
+    buttonContact.addEventListener("mouseleave", () => {
+        mouseCursor.classList.remove("link-grow")
+    })
+    buttonContact.addEventListener("mouseover", () => {
+        mouseCursor.classList.add("link-grow")
+    })
+}
+//Navbar
+// const page = document.querySelector("#body")
+// const menuToggle = document.querySelector(".menu_toggle")
+// const content = document.querySelector(".main")
+
+// menuToggle.addEventListener("click", () => {
+//     page.classList.toggle("shazam")
+// })
+// content.addEventListener("click", () => {
+//     page.classList.remove('shazam')
+// })
+
+//Mobile Issue
+// const mobileHelp = document.getElementById("mobile_help")
+// var disp = mobileHelp.style.display
+// if (disp === "none" || disp === "") {
+// console.log("This")
+// }
+
+
--- routes/contact.js
+++ routes/contact.js
@@ -1,12 +1,80 @@
 const router = require("express").Router()
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 router.get("/", (req, res) => {
+
     res.render("contact/index", {
         title: "Contact",
         textColor: "header_color_2",
         mainStyle: "main_three",
         pageName: "contact"
     })
+})
+router.post("/", (req, res) => {
+    const { name, email, message } = req.body
+    let emailSend = process.env.GM_EMAIL
+
+    const msg = {
+        to: emailSend,
+        from: `Portfolio <workingeveryday2@gmail>`,
+        subject: "Contact Portfolio",
+        html: `
+            <table width="100%" border="0" cellspacing="0" cellpadding="0"
+        style="width:100%!important;line-height: 1.4;color: #00394d;">
+        <tr>
+            <td align="center">
+                <table width="600" border="0" cellspacing="0" cellpadding="40"
+                    style="border:1px solid #eaeaea;border-radius:5px;margin:40px 0;text-align:center">
+                    <tr>
+                        <td align="center">
+                            <table width="100%" cellpadding="0" cellspacing="0">
+                                <tr>
+                                    <td width="100%">
+                                        <table align="center" width="570" cellpadding="0" cellspacing="0" border="0">
+                                            <tr>
+                                                <td style="padding:10px 35px;">
+                                                    <h1 style="color: #004d66;">
+                                                        ${name}
+                                                    </h1>
+                                                    <h2 style="color: #007399;">
+                                                        ${email}
+                                                    </h2>
+                                                    <h3 style="color: #00ace6;">
+                                                        ${message}
+                                                    </h3>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+    `
+    }
+    sgMail.send(msg)
+    sgMail.send(msg).then(_ => {
+        res.redirect('/contact');
+    }).catch((error) => {
+        console.log('error', error);
+    });
+
+
+    // (async () => {
+    // try {
+    // await sgMail.send(msg);
+    // res.redirect("/contact")
+    // } catch (err) {
+    // console.error(err.toString());
+    // }
+    // })();
+
+    // res.redirect("contact")
 })
 
 module.exports = router
