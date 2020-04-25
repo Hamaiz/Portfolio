bad7929c1f40a78484999cc24c8c686be4254315
Hamaiz <hamaiz5502@gamil.com>
2020-04-25 18:40:22 +0500

    Email fixings

--- routes/contact.js
+++ routes/contact.js
@@ -90,101 +90,34 @@ router.post("/", [
         </table>
         `
 
-        let transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.GM_EMAIL,
-                pass: process.env.GM_PASSWORD
-            }
-        });
-
-        let mailOption = {
-            from: `"Portfolio" <${process.env.GM_EMAIL}>`,
-            to: process.env.GM_EMAIL,
-            subject: "Portfolio Contact",
+        const msg = {
+            to: "test-twnvm1o7q@srv1.mail-tester.com",
+            from: `Portfolio <${process.env.GM_EMAIL}>`,
+            subject: "Contact Portfolio",
             html: output
         }
-        transporter.sendMail(mailOption, (error, info) => {
-            if (error) {
+        sgMail
+            .send(msg)
+            .then(() => {
+                req.flash("success_msg", "Your message has been send. I'll contact you shortly")
+                res.redirect('/contact');
+                console.log(req.flash())
+            }, error => {
                 console.log(error)
-                req.flash("error_msg", "There was a problem while sending email")
-                res.redirect("/contact")
-            } else {
-                req.flash("success_msg", "Your message has been sent. I'll contact you shortly")
-                res.redirect("/contact")
-            }
-        })
-
-
-
-        //     const msg = {
-        //         to: process.env.GM_EMAIL,
-        //         from: `Portfolio <${process.env.GM_EMAIL}>`,
-        //         subject: "Contact Portfolio",
-        //         html: `
-        //         <table width="100%" border="0" cellspacing="0" cellpadding="0"
-        //     style="width:100%!important;line-height: 1.4;color: #00394d;">
-        //     <tr>
-        //         <td align="center">
-        //             <table width="600" border="0" cellspacing="0" cellpadding="40"
-        //                 style="border:1px solid #eaeaea;border-radius:5px;margin:40px 0;text-align:center">
-        //                 <tr>
-        //                     <td align="center">
-        //                         <table width="100%" cellpadding="0" cellspacing="0">
-        //                             <tr>
-        //                                 <td width="100%">
-        //                                     <table align="center" width="570" cellpadding="0" cellspacing="0" border="0">
-        //                                         <tr>
-        //                                             <td style="padding:10px 35px;">
-        //                                                 <h1 style="color: #004d66;">
-        //                                                     ${name}
-        //                                                 </h1>
-        //                                                 <h2 style="color: #007399;">
-        //                                                     ${email}
-        //                                                 </h2>
-        //                                                 <h3 style="color: #00ace6;">
-        //                                                     ${message}
-        //                                                 </h3>
-        //                                             </td>
-        //                                         </tr>
-        //                                     </table>
-        //                                 </td>
-        //                             </tr>
-        //                         </table>
-        //                     </td>
-        //                 </tr>
-        //             </table>
-        //         </td>
-        //     </tr>
-        // </table>
-        // `
-        //     }
-        //     // sgMail.send(msg)
-        //     sgMail
-        //         .send(msg)
-        //         .then(() => {
-        //             req.flash("success_msg", "Your message has been send. I'll contact you shortly")
-        //             res.redirect('/contact');
-        //             console.log(req.flash())
-        //         }, error => {
-        //             console.log(error)
-        //             if (error.response) {
-
-        //                 const errorMessages = [{ msg: "There was an error sending the message. Please try again" }]
-        //                 res.render("contact/index", {
-        //                     title: "Contact",
-        //                     textColor: "header_color_2",
-        //                     mainStyle: "main_three",
-        //                     pageName: "contact",
-        //                     errors: errorMessages,
-        //                     name,
-        //                     email,
-        //                     message
-        //                 })
-        //             }
-        //         });
+                if (error.response) {
+                    const errorMessages = [{ msg: "There was an error sending the message. Please try again" }]
+                    res.render("contact/index", {
+                        title: "Contact",
+                        textColor: "header_color_2",
+                        mainStyle: "main_three",
+                        pageName: "contact",
+                        errors: errorMessages,
+                        name,
+                        email,
+                        message
+                    })
+                }
+            });
     }
 })
 
